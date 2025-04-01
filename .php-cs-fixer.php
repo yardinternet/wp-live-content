98677b268a787a95972dbe007e7b2a90bada76c0
Simon <simon@yard.nl>
2025-04-01 09:38:23 +0200

    Initial commit

---
+++ .php-cs-fixer.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Finder;
+use Yard\PhpCsFixerRules\Config;
+
+# PHP CS Fixer can be run by using the composer script `composer format`
+
+$finder = Finder::create()
+	->in(__DIR__)
+	->name('*.php')
+	->notName('*.blade.php')
+	->ignoreDotFiles(true)
+	->ignoreVCS(true)
+	->exclude('public')
+	->exclude('node_modules')
+	->exclude('build')
+	->append(['.php-cs-fixer.php']);
+
+return Config::create($finder);
---
+++ resources/views/example.blade.php
@@ -0,0 +1,1 @@
+{{ app(\Yard\SkeletonPackage\Example::class)->getQuote() }}
