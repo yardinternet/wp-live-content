ddb7d199cfa80ae90bb998bf8aab918606d808e1
Simon <simon@yard.nl>
2025-04-01 17:38:44 +0200

    (fix): route url

--- resources/views/components/live-content.blade.php
+++ resources/views/components/live-content.blade.php
@@ -1,9 +1,9 @@
 <div id="live-content">
     <div
-        hx-ext="sse"
-        sse-connect="{{ route('yard.live-content.stream', ['id' => $postId]) }}"
-        sse-swap="update"
-        sse-close="close">
+            hx-ext="sse"
+            sse-connect="/yard/live-content/stream?id={{ $postId }}"
+            sse-swap="update"
+            sse-close="close">
     </div>
     {!! apply_filters('the_content', get_the_content(null, false, $postId)) !!}
 </div>
