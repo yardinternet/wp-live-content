c9e62c4970445a338379a329f85fd740da441cb8
Simon <simon@yard.nl>
2025-04-01 17:38:44 +0200

    (fix): route url

--- resources/views/partials/notification.blade.php
+++ resources/views/partials/notification.blade.php
@@ -1,7 +1,7 @@
 <button
-    hx-post="{{ route('yard.live-content.content', ['id' => $postId]) }}"
-    hx-swap="outerHTML"
-    hx-target="#live-content"
-    class="w-full bg-primary text-white p-4 text-center">
+        hx-post="/yard/live-content/content?id={{ $postId }}"
+        hx-swap="outerHTML"
+        hx-target="#live-content"
+        class="w-full bg-primary text-white p-4 text-center">
     Er is 1 nieuwe update, klik om te herladen
 </button>
