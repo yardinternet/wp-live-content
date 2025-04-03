98badb805a4fdbee4489cd6773120f2b276953b7
Simon <simon@yard.nl>
2025-04-03 16:44:31 +0200

    (fix): update button class

--- resources/views/partials/notification.blade.php
+++ resources/views/partials/notification.blade.php
@@ -2,6 +2,6 @@
         hx-post="/yard/live-content/content?id={{ $postId }}"
         hx-swap="outerHTML"
         hx-target="#live-content"
-        class="w-full bg-primary text-white p-4 text-center">
+        class="wp-live-content w-full bg-primary text-white p-4 text-center">
     Er is 1 nieuwe update, klik om te herladen
 </button>
