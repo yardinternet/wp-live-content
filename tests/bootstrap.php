45a58644a3792c7add023e7e09d21f8339e53d03
Simon <simon@yard.nl>
2026-08-04 16:51:00 +0200

    fix: expire push timestamp transient after a day

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,7 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 
 WP_Mock::setUsePatchwork(true);
 WP_Mock::bootstrap();
+
+if (! defined('DAY_IN_SECONDS')) {
+	define('DAY_IN_SECONDS', 86400);
+}
